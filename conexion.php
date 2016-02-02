c74ba113ee7c9936d7f6bcc235712f80d2dded17
Christean <christean@users.noreply.github.com>
2016-02-02 01:16:14 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,28 +3,13 @@
 # Type="MYSQL"
 # HTTP="true"
 /* php 5.5*/
-$hostname = "localhost";
-$database = "geofel";
-$username = "root";
-$password = "root";
+$hostname = "$host";
+$database = "$db";
+$username = "$user";
+$password = "$pwd";
 $con =  mysql_connect($hostname, $username, $password);
 mysql_select_db($database,$con);
 if (!$con) {
     die('No pudo conectarse: ' . mysql_error());
 }
-
-
-/*
-$hostname = "localhost";
-$database = "grupo_sizek";
-$username = "sag";
-$password = "sag";
-$con =  mysqli_connect($hostname, $username, $password);
-mysqli_select_db($con, $database);
-if (!$con) {
-    die('No pudo conectarse: ' . mysqli_error());
-}else{
-	//echo "conectado";
-}
-*/
 ?> 
